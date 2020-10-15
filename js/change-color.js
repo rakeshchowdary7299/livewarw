ff0f4bfc023055e2fdc63a67a9ca0c8f11aa9390
rakeshchowdary <rakeshchowdary7299@gmail.com>
2020-10-15 10:09:58 -0700

    first commit

---
+++ js/change-color.js
@@ -0,0 +1,8 @@
+/* setInterval(function() {
+    $('li').fadeOut(500, function() {
+        var $this = $(this);
+          
+        $this.toggleClass('first second');        
+        $this.fadeIn(500);
+    });
+}, 3000); */
---
+++ js/new-color-change.js
@@ -0,0 +1,9 @@
+<script>
+2
+jQuery(document).ready(function($) {
+3
+$('.header-bottom').invertImgText();
+4
+});
+5
+</script>
